64e9fcf1de22bf722b898dd7d08031a54a6dd77f
tranhieu240503 <tranhieukiss2105@gmail.com>
2023-12-20 19:52:16 +0700

    web

---
+++ src/components/designLayouts/buttons/ShopNow.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ShopNow = () => {
+  return (
+    <button className="bg-primeColor text-red text-lg font-bodyFont w-[185px] h-[50px] hover:bg-green duration-300 font-bold radius-500">
+      Shop Now
+    </button>
+  );
+};
+
+export default ShopNow;
